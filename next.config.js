b939ad56a8e3fe3224cbf862b3e8032a9f4afee4
kimhecan <kimhecan@rainist.com>
2023-01-05 21:47:29 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const { NEXT_PUBLIC_NAMESPACE_ENV, NEXT_PUBLIC_COMMIT_SHA } = process.env;
+
+const isNamespaceProduction = NEXT_PUBLIC_NAMESPACE_ENV === 'production';
+
+const getAssetPrefix = () => {
+  if (!isNamespaceProduction) {
+    return '';
+  } else {
+    return  `https://next-example.s3.amazonaws.com/${NEXT_PUBLIC_COMMIT_SHA}`;
+  }
+};
+
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: getAssetPrefix(),
+}
